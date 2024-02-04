1962a3523b76c5e9eaf6f23da24de048baafdf24
Beeplex64 <madphone0024@gmail.com>
2024-02-04 16:13:59 +0900

    refactoring sleep code

--- pal_starter.py
+++ pal_starter.py
@@ -4,6 +4,7 @@ import mcrcon
 import time
 import threading
 
+
 server_address = "127.0.0.1"     # マルチプレイするときに入れるアドレス
 with open('./pass.txt', 'r') as passwd:  # apss.txtからパスワードを読み取る
     server_pass = passwd.readline().rstrip('\r\n').rstrip('\n')  # パスワード
@@ -15,17 +16,15 @@ pal_start = ("/home/deck/Desktop/steamcmd/palworld/PalServer.sh "
 pal_stop = "Shutdown 30 !!!MEMORY_LEAK_REBOOT_IS_SCHEDULED_IN_30_SECONDS!!!"
 
 
-def start_server():
-    print("[INFO]Initial Pal server start")
-
+def log_output():
     while True:
         line = server_proc.stdout.readline().decode('utf8', 'replace')
         if line:
             print(f"[Server_log]{line}", end='')
 
-
+print("[INFO]Initial Pal server start")
 server_proc = subprocess.Popen(pal_start, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-thread1 = threading.Thread(target=start_server,)  # サーバーの標準出力を出すスレッドを作成
+thread1 = threading.Thread(target=log_output,)  # サーバーの標準出力を出すスレッドを作成
 thread1.start()  # サーバーの標準出力を出すスレッドを実行
 
 counter = 0
@@ -40,21 +39,17 @@ while True:
             log = mcr.command(pal_stop)
             print(log)
             # server_proc.terminate()
-        time.sleep(10)
-        print("[INFO]sleep 10s")
-        time.sleep(10)
-        print("[INFO]sleep 20s")
-        with mcrcon.MCRcon(server_address, server_pass, server_port) as mcr:
-            log = mcr.command("Save")
-            print(log)
-        time.sleep(10)
-        print("[INFO]sleep 30s")
-        time.sleep(10)
-        print("[INFO]sleep 40s")
-        time.sleep(10)
-        print("[INFO]sleep 50s")
-        time.sleep(10)
-        print("[INFO]sleep 60s end sleep")
+        for i in range(6):
+            time.sleep(10)
+            sec_wait = str((i+1)*10)
+            if i <= 4:
+                print(f"[INFO]sleep {sec_wait}s")
+            else:
+                print(f"[INFO]sleep {sec_wait}s end sleeping")
+            if i == 1:
+                with mcrcon.MCRcon(server_address, server_pass, server_port) as mcr:
+                    log = mcr.command("Save")
+                    print(log)
         print("[INFO]Reboot Pal server start")
         server_proc = subprocess.Popen(pal_start, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         counter = 0
